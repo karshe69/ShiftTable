0b63827ffca01514ed09dc244dd481fd7ecdfa96
KarshE69 <yarden.karsh@gmail.com>
2022-09-05 13:08:35 +0300

    heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const admin = require('firebase-admin')
 const express = require('express')
 const cors = require('cors')
-const port = 3000
+const port = process.env.PORT || 3000
 const app = express()
 app.use(cors({ origin: true }))
 
