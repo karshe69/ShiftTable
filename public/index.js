71086b2773cf5e6c66177636817a67922935c619
KarshE69 <yarden.karsh@gmail.com>
2022-10-15 04:22:48 +0300

    commit

--- public/index.js
+++ public/index.js
@@ -3,28 +3,53 @@ import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/
 import { getFirestore, doc, collection, getDocs, getDoc, where, query } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-firestore.js"
 const db = getFirestore(app);
 
+//consts for design:
+const tableElemSize = 128
+
 document.getElementById("logout-btn-auth").addEventListener("click", logout);
 
-const tablesGrid = document.getElementById("tables")
-
-// let arr = ["table1", "table2"]
-// for (let i = 0; i < arr.length; i++) {
-//   const tableBox = document.createElement("a")
-//   tableBox.innerText = `${arr[i]}`
-//   tablesGrid.appendChild(tableBox)
-// }
-
-
 async function getTable(uid) {
-  const table = await getDoc(doc(db, 'tables', 'j1B6I18T0VfcaDFdo2Ep'))
-  console.log(table.id, " => ", table.data());
-
   const tables = collection(db, "tables")
-  let q = query(tables, where("write", "array-contains-any", [uid]))
-  let querySnapshot = await getDocs(q)
-  querySnapshot.forEach((doc) => {
-    console.log(doc.id, " => ", doc.data());
-  });
+  let viewQuery = query(tables, where("read", "array-contains-any", [uid]))
+  let viewSnapshot = await getDocs(viewQuery)
+  const viewTables = document.getElementById("viewTables")
+
+  if (viewSnapshot.empty) {
+    viewTables.appendChild(createTableElem(null, null))
+  }
+  else {
+    viewSnapshot.forEach((doc) => {
+      viewTables.appendChild(viewTables.appendChild(createTableElem(doc.data()["name"], doc.id)))
+    });
+  }
+
+
+  let adminQuery = query(tables, where("write", "array-contains-any", [uid]))
+  let adminSnapshot = await getDocs(adminQuery)
+  const adminTables = document.getElementById("adminTables")
+
+  if (adminSnapshot.empty) {
+    viewTables.appendChild(createTableElem(null, null))
+  }
+  else {
+    for (let index = 0; index < 10; index++) {
+      adminSnapshot.forEach((doc) => {
+        adminTables.appendChild(viewTables.appendChild(createTableElem(doc.data()["name"], doc.id)))
+      });
+    }
+  }
+}
+
+function createTableElem(name, id) {
+  const tableBox = document.createElement("a")
+  if (name == null) {
+    tableBox.innerText = `Nothing To See Here`
+    return tableBox
+  }
+  tableBox.innerText = name
+  tableBox.href = `/table/${id}`
+  tableBox.className = "flex items-center justify-center bg-slate-100 hover:scale-105 ease-in-out min-w-[" + tableElemSize + "px] w-[" + tableElemSize + "px] h-[" + tableElemSize + "px]"
+  return tableBox
 }
 
 
