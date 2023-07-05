58b6c836c06d9373d1ef186e1d80f67217fdfbf5
KarshE69 <yarden.karsh@gmail.com>
2023-07-05 20:13:43 +0300

    updated to radio buttons

--- app/table/[id]/editPeople.jsx
+++ app/table/[id]/editPeople.jsx
@@ -39,23 +39,33 @@ export function EditPeople({ children }) {
                 <div className='flex-1 text-sm p-2 flex flex-col justify-center items-center'>
                     <h1 className='font-extrabold text-3xl uppercase p-5'>Edit People</h1>
                     <div className="flex justify-between space-x-8 w-full px-4">
-                        <div className="py-5 justify-center flex w-full space-y-2 bg-slate-300 p-6 rounded-lg">
+                        <div className="justify-center flex w-full space-y-2 bg-slate-300 py-6 px-4 rounded-lg">
                             <EditPerson>{{ person }}</EditPerson>
                         </div>
                         <div className="justify-center space-y-2 bg-slate-300 p-6 rounded-lg">
                             <h2 className="text-2xl whitespace-nowrap font-bold">List Of People</h2>
-                            {people.map((person, i) => (
-                                <h3 key={i} onClick={(e) => clickPerson(i)} className="text-xl rounded-lg border-2 border-slate-300 p-2 font-bold bg-slate-600 hover:bg-slate-700 text-white whitespace-nowrap">{person.docname}</h3>
-                            ))}
-                            <div onClick={(e) => newPerson()} className="text-xl rounded-lg border-2 border-slate-300 p-2 font-bold bg-slate-600 hover:bg-slate-700 text-white whitespace-nowrap flex flex-nowrap">
-                                <i className="fa-solid fa-plus pr-2 pt-1"></i>
-                                <h3>New Person</h3>
-                            </div>
+                            <ul className="grid w-full gap-6 md:grid-rows-2">
+                                {people.map((person, i) => (
+                                    // <h3 key={i} onClick={(e) => clickPerson(i)} className="text-xl rounded-lg border-2 border-slate-300 p-2 font-bold bg-slate-600 hover:bg-slate-700 text-white whitespace-nowrap">{person.docname}</h3>
+                                    <li key={i} onClick={(e) => clickPerson(i)}>
+                                        <input type="radio" id={"person" + i} name="hosting" value={"person" + i} className="hidden peer" required></input>
+                                        <label for={"person" + i} className="text-lg bg-slate-600 text-white font-semibold inline-flex items-center justify-between w-full p-3 border-2 border-transparent rounded-lg cursor-pointer peer-checked:border-slate-800 peer-checked:bg-slate-500 hover:bg-slate-700">{person.docname}</label>
+                                    </li>
+                                ))}
+                                <li onClick={(e) => newPerson()}>
+                                    <input type="radio" id={"newperson"} name="hosting" value={"newperson"} className="hidden peer" required></input>
+                                    <label for={"newperson"} className="text-lg bg-slate-600 text-white font-semibold inline-flex items-center justify-between w-full p-3 border-2 border-transparent rounded-lg cursor-pointer peer-checked:border-slate-800 peer-checked:bg-slate-500 hover:bg-slate-700">
+                                        <i className="fa-solid fa-plus pr-2 pt-1"></i>
+                                        <h3>New Person</h3>
+                                    </label>
+                                </li>
+                            </ul>
+
                         </div>
                     </div>
                     <button onClick={submitHandler} className="mt-4 bg-slate-800 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded">finish</button>
                 </div>
-            </div>
-        </div>
+            </div >
+        </div >
     )
 }
--- app/table/[id]/editPerson.jsx
+++ app/table/[id]/editPerson.jsx
@@ -12,16 +12,30 @@ export function EditPerson({ children }) {
     }, [person.docname])
 
     return (
-        <div className="text-lg p-2 justify-center items-center">
-            <div>
-                <i class="fa-solid fa-pen-to-square text-sm"></i>
+        <div className="text-lg justify-center items-center w-full relative">
+            <>
+
+            </>
+            <div className="justify-center">
+                <i className="fa-solid fa-pen-to-square text-sm"></i>
                 <input type="string" value={name} onChange={(e) => setName(e.target.value)} placeholder='name' className='outline-none bg-inherit text-slate-900 placeholder:text-slate-600 p-2 text-left w-auto inline-block font-bold' />
             </div>
-            <div>
-                <h2>permenant reservations</h2>
-                <></>
+            <div className="flex w-full justify-between h-5/6 py-4 px-2">
+                <div className="px-4 flex justify-between items-center bg-slate-400 rounded w-full">table</div>
+                <div className="flex-row pl-4 justify-end space-y-6">
+                    <ul className="grid w-full gap-6 md:grid-rows-2">
+                        <li>
+                            <input type="radio" id="hosting-small" name="hosting" value="hosting-small" className="hidden peer" required></input>
+                            <label for="hosting-small" className="text-lg bg-slate-600 text-white font-semibold inline-flex items-center justify-between w-full p-5 border-4 border-transparent rounded-lg cursor-pointer peer-checked:border-slate-800 peer-checked:bg-slate-500 hover:bg-slate-700">permanent reservs</label>
+                        </li>
+                        <li>
+                            <input type="radio" id="hosting-big" name="hosting" value="hosting-big" className="hidden peer"></input>
+                            <label for="hosting-big" className="text-lg bg-slate-600 text-white font-semibold inline-flex items-center justify-between w-full p-5 border-4 border-transparent rounded-lg cursor-pointer peer-checked:border-slate-800 peer-checked:bg-slate-500 hover:bg-slate-700">temporary reservs</label>
+                        </li>
+                    </ul>
+                </div>
+
             </div>
-            <div>temp</div>
         </div>
     )
 }
